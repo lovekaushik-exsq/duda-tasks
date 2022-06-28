5c8d6c13c8804db47cc393f6700378e402a17403
Lalit Singh <lsingh@ex2india.com>
2022-06-28 15:38:58 +0530

    lalit commit

---
+++ responsive-sticky-navbar-master/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+//  for Scrolling Effect <dont copy code learn it >
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+    else {
+          $('nav').removeClass('black');
+    }
+})
