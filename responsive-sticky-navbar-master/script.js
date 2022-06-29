b17d84e00a2d68f38aaca39d9dd687ee09afa9fd
Love Kaushik <lkaushik@ex2india.com>
2022-06-29 17:05:40 +0530

    changes for dynamic loading

--- responsive-sticky-navbar-master/script.js
+++ responsive-sticky-navbar-master/script.js
@@ -1,22 +1,25 @@
 const sectionIds = [];
-const idsToGap = [];
+const gap = document.getElementById("nav").offsetHeight;
+
 const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
   sectionIds.push(section.id);
 });
-const topGap = document.getElementById(sectionIds[0]).offsetTop;
-const navGap = document.getElementById("nav").offsetTop;
-const gap = topGap - navGap;
-sectionIds.forEach((id) => {
-  const gapFromSection = document.getElementById(id).offsetTop - gap;
-  idsToGap.push(gapFromSection);
-});
+
+let idsToGap = [];
+
 handleClickScroll();
+history.scrollRestoration = "manual";
 
 $(document).ready(function () {
   $(window).scroll(function () {
     let nav = document.querySelector("#nav");
     let elements = nav.querySelectorAll("button");
+    idsToGap = [];
+    sectionIds.forEach((id) => {
+      const gapFromSection = document.getElementById(id).offsetTop - gap;
+      idsToGap.push(gapFromSection);
+    });
     if (window.pageYOffset < idsToGap[0]) {
       elements.forEach((element) => {
         element.classList.remove("active");
@@ -31,7 +34,7 @@ function makeTabActive(currentPoint, gapArr, elements) {
   gapArr.forEach((value, key) => {
     const variableChange = getBuffer(
       currentPoint,
-      value - (gapArr[key - 1] || 0)
+      sections[key].offsetHeight || 0
     );
     if (variableChange > value) {
       elements[key].classList.add("active");
@@ -43,11 +46,11 @@ function makeTabActive(currentPoint, gapArr, elements) {
 function getBuffer(currentPoint, value) {
   let ans;
   if (value < 100) {
-    ans = currentPoint + value / 45;
-  } else if (value < 300) {
     ans = currentPoint + value / 30;
+  } else if (value < 300) {
+    ans = currentPoint + value / 20;
   } else {
-    ans = currentPoint + value / 15;
+    ans = currentPoint + value / 10;
   }
   return ans;
 }
@@ -61,6 +64,11 @@ function removeRest(value, arr, elements) {
 }
 
 function handleClickScroll() {
+  idsToGap = [];
+  sectionIds.forEach((id) => {
+    const gapFromSection = document.getElementById(id).offsetTop - gap;
+    idsToGap.push(gapFromSection);
+  });
   let nav = document.querySelector("#nav");
   let elements = nav.querySelectorAll("button");
   elements.forEach((element, index) => {
@@ -69,36 +77,3 @@ function handleClickScroll() {
     });
   });
 }
-
-// let scrollDirection = this.oldScroll < this.scrollY;
-// this.oldScroll = this.scrollY;
-// const firstSectionTopGap = document.getElementById("first").offsetTop - gap;
-// const secondSectionTopGap = document.getElementById("second").offsetTop - gap;
-// const thirdSectionTopGap = document.getElementById("third").offsetTop - gap;
-
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     var nav = document.querySelector("#nav");
-//     var element = nav.querySelectorAll("a");
-//     if (window.pageYOffset < firstSectionTopGap) {
-//       element[0].classList.remove("active");
-//       element[1].classList.remove("active");
-//       element[2].classList.remove("active");
-//     }
-//     if (window.pageYOffset > firstSectionTopGap) {
-//       element[0].classList.add("active");
-//       element[1].classList.remove("active");
-//       element[2].classList.remove("active");
-//     }
-//     if (window.pageYOffset > secondSectionTopGap) {
-//       element[0].classList.remove("active");
-//       element[1].classList.add("active");
-//       element[2].classList.remove("active");
-//     }
-//     if (window.pageYOffset > thirdSectionTopGap) {
-//       element[0].classList.remove("active");
-//       element[1].classList.remove("active");
-//       element[2].classList.add("active");
-//     }
-//   });
-// });
