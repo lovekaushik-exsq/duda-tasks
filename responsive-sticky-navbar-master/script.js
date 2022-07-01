e80c29e13f6b54e9659ec44b878d5019cbde0d6d
Love Kaushik <lkaushik@ex2india.com>
2022-07-01 18:10:29 +0530

    code made more modular

--- responsive-sticky-navbar-master/script.js
+++ responsive-sticky-navbar-master/script.js
@@ -1,25 +1,41 @@
 //If refreshed it will bring site to top
 history.scrollRestoration = "manual";
-
-let idsToGap = [];
-const sectionIds = [];
-
 //Nav height calculate
-const gap = document.getElementById("nav").offsetHeight;
+const gap = document.querySelector("#nav").offsetHeight;
 
+//function to find section ids
+const sectionIds = [];
 const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
   sectionIds.push(section.id);
 });
 
-//Clicking tabs
-handleClickScroll();
-function handleClickScroll() {
-  idsToGap = [];
+//function to find section gap from nav
+function getSectionGapFromTop() {
+  let idsToGap = [];
   sectionIds.forEach((id) => {
     const gapFromSection = document.getElementById(id).offsetTop - gap;
     idsToGap.push(gapFromSection);
   });
+
+  return idsToGap;
+}
+
+//on ready of document
+document.addEventListener("DOMContentLoaded", function () {
+  handleClickScroll();
+  handleScrolling();
+  handleLinkWithIdsScroll();
+});
+//on change of id in link
+window.addEventListener("hashchange", () => {
+  handleLinkWithIdsScroll();
+});
+
+//Clicking tabs
+handleClickScroll();
+function handleClickScroll() {
+  const idsToGap = getSectionGapFromTop();
   let nav = document.querySelector("#nav");
   let elements = nav.querySelectorAll("button");
   elements.forEach((element, index) => {
@@ -29,34 +45,22 @@ function handleClickScroll() {
   });
 }
 
-//Coming on link with #
-$(document).ready(function () {
+//Handle link with ids
+function handleLinkWithIdsScroll() {
   if (document.URL.indexOf("#") > 0) {
     var link = document.URL.split("#");
-    scrollId("#" + link[1]);
+    let id = "#" + link[1];
+    let y = document.querySelector(id).offsetTop - gap;
+    window.scrollTo(0, y);
   }
-});
-$(window).on("hashchange", function (e) {
-  if (document.URL.indexOf("#") > 0) {
-    var link = document.URL.split("#");
-    scrollId("#" + link[1]);
-  }
-});
-
-function scrollId(id) {
-  window.scrollTo(0, $(id).offset().top - gap + 1);
 }
 
 //Scrolling
-$(document).ready(function () {
-  $(window).scroll(function () {
+function handleScrolling() {
+  window.addEventListener("scroll", function () {
     let nav = document.querySelector("#nav");
     let elements = nav.querySelectorAll("button");
-    idsToGap = [];
-    sectionIds.forEach((id) => {
-      const gapFromSection = document.getElementById(id).offsetTop - gap;
-      idsToGap.push(gapFromSection);
-    });
+    const idsToGap = getSectionGapFromTop();
     if (window.pageYOffset < idsToGap[0]) {
       elements.forEach((element) => {
         element.classList.remove("active");
@@ -65,8 +69,9 @@ $(document).ready(function () {
       makeTabActive(window.pageYOffset, idsToGap, elements);
     }
   });
-});
+}
 
+//Add class active
 function makeTabActive(currentPoint, gapArr, elements) {
   gapArr.forEach((value, key) => {
     const variableChange = getBuffer(
@@ -76,6 +81,15 @@ function makeTabActive(currentPoint, gapArr, elements) {
     if (variableChange > value) {
       elements[key].classList.add("active");
       removeRest(value, gapArr, elements);
+    }
+  });
+}
+
+//Remove class active
+function removeRest(value, arr, elements) {
+  arr.forEach((ele, i) => {
+    if (value != ele) {
+      elements[i].classList.remove("active");
     }
   });
 }
@@ -90,12 +104,4 @@ function getBuffer(currentPoint, value) {
     ans = currentPoint + value / 10;
   }
   return ans;
-}
-
-function removeRest(value, arr, elements) {
-  arr.forEach((ele, i) => {
-    if (value != ele) {
-      elements[i].classList.remove("active");
-    }
-  });
 }
